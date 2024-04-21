e669bd65ea9602c9148f972ac060e7130be48477
Phillip Peterson <58966732+r-adaR@users.noreply.github.com>
2024-04-21 01:35:18 -0700

    Applied text generation to frontend.

--- LAHacks2024Project/api/suggestions.py
+++ LAHacks2024Project/api/suggestions.py
@@ -5,6 +5,7 @@ _types: list = ["recycling", "market", "metro", "park", "none"]
 
 class SuggestionState(rx.State):
     selected: str = "none"
+    response = ""
     form_data: dict = ()
 
     def handle_submit(self, form_data: dict):
@@ -15,20 +16,20 @@ class SuggestionState(rx.State):
     def generate_suggestions(self):
         match self.selected:
             case "recycling":
-                response = model.generate_content(f'List the top three best recycling centers in {self.form_data["city"]} {self.form_data["state"]} along with where they are at. If the city does not exist just output N/A.')
-                print(response.text)
+                response = model.generate_content(f'List the top three best recycling centers in {self.form_data["city"]} {self.form_data["state"]} along with where they are at.')
+                self.response = response.text
                 return None
             case "market":
-                response = model.generate_content(f'List the top three best farmers markets in {self.form_data["city"]} {self.form_data["state"]} along with where they are at. If the city does not exist just output N/A.')
-                print(response.text)
+                response = model.generate_content(f'List the top three best farmers markets in {self.form_data["city"]} {self.form_data["state"]} along with where they are at.')
+                self.response = response.text
                 return None
             case "metro":
-                response = model.generate_content(f'List the top three best public transit options in {self.form_data["city"]} {self.form_data["state"]} along with where they are at. If the city does not exist just output N/A.')
-                print(response.text)
+                response = model.generate_content(f'List the top three best public transit options in {self.form_data["city"]} {self.form_data["state"]} along with where they are at.')
+                self.response = response.text
                 return None
             case "park":
-                response = model.generate_content(f'List the top three best outdoor parks in {self.form_data["city"]} {self.form_data["state"]} along with where they are at. If the city does not exist just output N/A.')
-                print(response.text)
+                response = model.generate_content(f'List the top three best outdoor parks in {self.form_data["city"]} {self.form_data["state"]} along with where they are at.')
+                self.response = response.text
                 return None
             case _:
                 print("N/A")
